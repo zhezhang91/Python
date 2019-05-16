c411ec187d3d89359970f1fc47324edb017470d5
zhezhang91 <zz188659@gmail.com>
2019-05-16 10:50:12 -0400

    add play game logic

--- main.py
+++ main.py
@@ -85,8 +85,7 @@ def take_bet(chips):
 
 
 def hit(deck, hand):
-    single_card = deck.deal()
-    hand.add_card()
+    hand.add_card(deck.deal())
     hand.adjust_for_ace()
 
 
@@ -151,3 +150,64 @@ def dealer_wins(player, dealer, chips):
 
 def push(player, dealer):
     print('Dealer and Player tie! PUSH')
+
+
+while True:
+
+    print('WELCOME TO BLACKJACK')
+
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    player_chips = Chips()
+
+    take_bet(player_chips)
+
+    show_some(player_hand, dealer_hand)
+
+    while playing:
+
+        hit_or_stand(deck, player_hand)
+
+        show_some(player_hand, dealer_hand)
+
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
+
+            break
+    if player_hand.value <= 21:
+        while dealer_hand.value < player_hand.value:
+            hit(deck, dealer_hand)
+
+        show_all(player_hand, dealer_hand)
+
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+
+        else:
+            push(player_hand, dealer_hand)
+
+    print("\nPlayer's winnings stand at", player_chips.total)
+
+    # Ask to play again
+    new_game = input("Would you like to play another hand? Enter 'y' or 'n' ")
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print("Thanks for playing!")
+        break
